795d79c97affde5ce66c025002be0057dc506432
Luigi Battaglioli <him@theluigi.com>
2024-12-02 20:45:31 -0500

    Added git blame and harpoon

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,7 @@ return {
 	lazy = false,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "auto", -- latte, frappe, macchiato, mocha
+			flavour = "latte", -- latte, frappe, macchiato, mocha
 			background = { -- :h background
 				light = "latte",
 				dark = "mocha",
